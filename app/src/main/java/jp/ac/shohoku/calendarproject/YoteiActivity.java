9161e2b32fc323113eb47e8ae4ac62bf13570c3d
s19B712 <s19b712@shohokuac.jp>
2021-01-22 10:12:44 +0900

    決定ボタン途中

--- app/src/main/java/jp/ac/shohoku/calendarproject/YoteiActivity.java
+++ app/src/main/java/jp/ac/shohoku/calendarproject/YoteiActivity.java
@@ -2,7 +2,6 @@ package jp.ac.shohoku.calendarproject;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,14 +14,17 @@ import java.util.Calendar;
 
 public class YoteiActivity extends Activity {
 
-    private Button modorubutton, showdate;
-    private TextView nodate;
+    private Button modorubutton, decButton, showdate;
+    private TextView nodate, datet;
+    private EditText tit = null;
+    private EditText memot = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shinki);
 
+        // 戻るボタン処理
         // ボタンを押したときにイベントを取得できるようにする
         Button modorubutton = (Button) findViewById(R.id.button);
         modorubutton.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +39,24 @@ public class YoteiActivity extends Activity {
             }
         });
 
+        // 決定ボタン処理
+        EditText tit = (EditText)findViewById(R.id.titletext);
+        TextView datet = (TextView)findViewById(R.id.nodate);
+        EditText memot = (EditText)findViewById(R.id.memotext);
+        Button decButton = (Button) findViewById(R.id.button3);
+        decButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(YoteiActivity.this, MainActivity.class);
+                //intent.putExtra("title", tit.getText().toString());
+                //intent.putExtra("hiduke", datet.getText().toString());
+                //intent.putExtra("memo", memot.getText().toString());
+            }
+        });
+
+
+
+        // 日付選択
         showdate = (Button) findViewById(R.id.showdate);
         nodate = (TextView) findViewById(R.id.nodate);
         showdate.setOnClickListener(new View.OnClickListener() {
