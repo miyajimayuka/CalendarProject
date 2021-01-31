0d813f6074894e4d6652a3384176e4542f747873
s19B712 <s19b712@shohokuac.jp>
2021-01-31 15:57:42 +0900

    Todo完成

--- app/src/main/java/jp/ac/shohoku/calendarproject/Adapter.java
+++ app/src/main/java/jp/ac/shohoku/calendarproject/Adapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
--- app/src/main/java/jp/ac/shohoku/calendarproject/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/calendarproject/MainActivity.java
@@ -2,20 +2,13 @@ package jp.ac.shohoku.calendarproject;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.icu.text.CaseMap;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
-import java.util.Calendar;
 
 
 public class MainActivity extends Activity {
